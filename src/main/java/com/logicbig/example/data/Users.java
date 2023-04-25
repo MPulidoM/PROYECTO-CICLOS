9ef6803ed8d2f1fb812d4f854dae30b88da09cea
JordyBautista10 <123812969+JordyBautista10@users.noreply.github.com>
2023-04-25 16:02:01 -0500

    Add files via upload

---
+++ src/main/java/com/logicbig/example/data/Users.java
@@ -0,0 +1,125 @@
+package com.logicbig.example.data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Users {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private String personalName;
+
+    private String profile;
+
+    private int age;
+
+    private String phone;
+
+    private String email;
+
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Users(String username, String password, String personalName, String profile, int age, String phone, String email) {
+        this.username = username;
+        this.password = password;
+        this.personalName = personalName;
+        this.profile = profile;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Users() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getPersonalName() {
+        return personalName;
+    }
+
+    public void setPersonalName(String personalName) {
+        this.personalName = personalName;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", personalName='" + personalName + '\'' +
+                ", profile='" + profile + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users that = (Users) o;
+        return Objects.equals(username, that.username) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+}
